4c8c86d6a8e445203c8aeb355322e4707e388b11
Deoxymp <adinnugroho854@gmail.com>
2025-05-07 08:57:00 +0700

    #6

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-# app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.rag_qa import tanya_mobil
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
-    return {"message": "API untuk rekomendasi mobil berdasarkan pertanyaan."}
+    return {"message": "API untuk rekomendasi mobil."}
 
 @app.get("/tanya")
 def tanya(pertanyaan: str):
